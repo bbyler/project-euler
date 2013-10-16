8ccb9466e7aeec0c1e0d91a5673a326e1b9184f4
Brett Byler <bbyler12@gmail.com>
2013-10-16 08:30:42 -0700

    updating problem_2

--- problem_2/fibonacci.go
+++ problem_2/fibonacci.go
@@ -1,36 +1,51 @@
 package main
 
-import "fmt"
-
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-    n := 0
-    f_prev_2 := 1
-    f_prev_1 := 1
-     
-    return func() int {
-        var result int
-        if n == 0 {
-            n++ 
-            result = f_prev_2
-        } else if n == 1 {
-            n++
-            result = f_prev_1
-        } else {
-            result = f_prev_1 + f_prev_2
-            f_prev_2 = f_prev_1
-            f_prev_1 = result
-        }
-        return result
-        
-    }
-}
+import (
+  "fmt"
+  "math"
+  "time"
+)
 
 func main() {
-    f := fibonacci()
-    for i := 0; i < 10; i++ {
-        fmt.Println(f())
-    }
+  // Finding the sum of even Fibonacci numbers up to 4,000,000
+  t0 := time.Now()
+  var N float64
+  N = 4000000
+  fmt.Println(fibonacci_sum(N))
+  t1 := time.Now()
+  fmt.Println("Finished in", t0.Sub(t1))
+}
 
+// FAST FIBONACCI
+func fast_fibonacci(n float64) (float64) {
+  var fib float64 
+  if n == 0 {
+    fib = 0
+  } else if n == 1 {
+    fib = 1
+  // One more case below, intended to mitigate stack overflow
+  } else if n == 2 {
+    fib = 1
+  } else {
+    if math.Mod(n, 2) == 0 {
+      n = n/2
+      fib = fast_fibonacci(n)*(2*fast_fibonacci(n + 1) - fast_fibonacci(n))
+    } else {
+      n = (n - 1)/2
+      fib = math.Pow(fast_fibonacci(n + 1), 2) + math.Pow(fast_fibonacci(n), 2)
+    }
+  }
+  return fib
+}
+
+func fibonacci_sum(N float64) (float64) {
+  var sum, i float64
+  for i = 3; i < N; {
+    if fast_fibonacci(i) >= 4000000 {
+      break
+    }
+    sum = sum + fast_fibonacci(i)
+    i = i + 3
+  }
+  return sum
 }
