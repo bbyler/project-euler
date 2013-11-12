a875fef395dbdbbfd82956f1114cbf7c3b8f25eb
Brett Byler <bbyler12@gmail.com>
2013-11-11 21:49:03 -0700

    scaffold for problem 3, problem 4

---
+++ problem_3/PrimeFactor.java
@@ -0,0 +1,21 @@
+public class PrimeFactor {
+    public static void main(String[] args){
+        int N;
+        // N = 600851475143
+        N = 13195;
+        // Largest prime factor of 13195: 29
+        System.out.println(largest_prime(N));
+    }
+
+    public int generate(int n) {
+
+    }
+
+    public int prime_sieve(int n) {
+
+    }
+
+    public int largest_prime(int N) {
+
+    }
+}
