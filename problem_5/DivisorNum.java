3153f2c18e04a3cd8aa7d7512084d036d9fc9eaf
Brett Byler <bbyler12@gmail.com>
2013-11-13 19:15:51 -0700

    Problem 5 complete

--- problem_5/DivisorNum.java
+++ problem_5/DivisorNum.java
@@ -1,5 +1,23 @@
 public class DivisorNum {
     public static void main(String[] args) {
+        // Pass in last integer in range (20 --> 1..20)
+        // System.out.println(divisor(10)); // Outputs 2520
+        System.out.println(divisor(20)); // Outputs 232792560  
+    }
 
+    public static int divisor(int endInt) {
+        int[] range = new int[endInt - 2];
+        for (int i = 0; i < endInt - 2; i++){
+          range[i] = i + 2;
+        }
+        // range = [2, 3, ..., endInt], has length endInt - 1
+        int N = 2;
+        for (int i = 0; i < range.length; i++){
+            if (N % range[i] != 0) {
+                N++;
+                i = 0;
+            }
+        }
+        return N;
     }
 }
